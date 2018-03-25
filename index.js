48ef0e3c9a8c7f41b3e3d2889f27f024b5d79b9a
spyk1e <32196400+spyk1e@users.noreply.github.com>
2018-03-25 16:32:57 +0200

    Added examples on index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var mongo = require('./mongo');
 
+//Testing the update
+/*
+mongo.MongoFindSender(".elizondo@enron.com", 2, function (result) {
+    console.log(result);
+});
 
+mongo.UpdateWithId("52af48b6d55148fa0c19a529", "Ken, I am a loyal Enron Employee for over 17 years and my opinion (however insignificant) is that we stand and fight and not take any buy out offer from anyone.\n\nI know that I speak for the rest of my team when I say lets stand and fight and not sell out to the first bidder who will not give us what we are really worth.  We are the best and continue to be the best in the energy world, that is my attitude every day that I proudly come to work.  \n\nPlease reconsider and take our lumps like Columbia Gas System, like Continental Airlines and get back into solvency the best way we know how to.  The past is past, lets pick up ourselves by our boot straps and get back into the fight and win this one.\n\nYou know that you can count on me and the rest of my team in ETS to stand by this decision to stand and fight.\n\nEver Loyal and willing to serve.\n\nRudy Elizondo (ETS).\n\nps If you need more inspirational speeches from me, I will be happy to oblige.\n\n\n\n This Is a TESTTTTTTTTTT number 3", function () {
+    console.log("done");
+    mongo.MongoFindSender(".elizondo@enron.com", 2, function (result) {
+        console.log(result);
+    });
+})
+*/
 
 /*
 mongo.MongoFindQuery({
